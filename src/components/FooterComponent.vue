1f347cb3230058ad5a635116c4e8b657b766cf01
safir-github <safirmuscu@gmail.com>
2024-01-26 00:48:38 +0100

    commit

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -6,19 +6,207 @@
 
     <footer>
 
+        <div class="copyright">
+            <p>© 2024 <span>O'</span>Clavier</p>
+
+        </div>
+
+        <div class="menu-footer">
+            <nav>
+                <a href="#">Politique de cookies</a>
+                <a class="conditions" href="#"><span>Conditions générales de vente</span></a>
+                <a class="mentions" href="#">Mentions légales et politique de confidentialité</a>
+            </nav>
+
+        </div>
+
+        <div class="reseaux">
+
+            <ul>
+                <li>
+                    <a href="#" style="--clr0: #ff6efb; --clr1: #ff6efb; --clr2: #ff2666a">
+                        <i class="fa-brands fa-instagram"></i>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#" style="--clr0: #00009A; --clr1: #00009A; --clr2: #ff2666a">
+                        <i class="fa-brands fa-snapchat"></i>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#" style="--clr0: #59d8c3; --clr1: #59d8c3; --clr2: #ff2666a">
+                        <i class="fa-brands fa-envelope"></i>
+                    </a>
+                </li>
+            </ul>
+
+        </div>
+
         
 
     </footer>
 
 </template>
 
-<style>
+<style scoped>
+
+@import url('https://fonts.googleapis.com/css2?family=Cardo&display=swap');
+
 
 footer {
-    background-color: #050816;
-    height: 200px;
+    background-color: black;
+    height: 8em;
     border-top: solid 1px white;
+    display: flex;
+    align-items: center;
     
 }
+
+.copyright {
+    width: 33%;
+    color: white;
+    font-family: 'cardo';
+    font-size: 0.9em;
+    display: flex;
+    align-items: center;
+    padding-left: 1.5em;
+}
+
+.copyright p span {
+    color: #ff6efb;
+}
+
+.copyright p:hover {
+    /* color: #ff6efb; */
+    transform: scale(1.3); /* Ajuste la valeur selon tes préférences */
+    transition: transform 1s ease; /* Ajoute une transition fluide */
+}
+
+.menu-footer {
+    width: 33%;
+    font-family: 'cardo';
+    font-size: 0.9em;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: row;
+
+}
+
+.menu-footer a {
+    text-decoration: none;
+    color: white;
+    display: block;
+    line-height: 2.5em;
+}
+
+.menu-footer a:hover {
+    /* color: #ff6efb; */
+    transform: scale(1.3); /* Ajuste la valeur selon tes préférences */
+    transition: transform 1s ease; /* Ajoute une transition fluide */
+}
+
+/* .conditions:hover {
+    color: #00009A;
+}
+
+.mentions:hover {
+    color: #5ED6C2;
+} */
+
+
+
+.reseaux {
+    width: 33%;
+    padding-right: 1.5em;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
+}
+
+.reseaux ul {
+    display: flex;
+    align-items: center;
+    margin-bottom: 2em;
+    gap: 100px;
+}
+
+.reseaux li {
+    list-style: none;
+}
+
+.fa-brands {
+    font-size: 20px;
+    color: #262626;
+    line-height: 40px;
+    transition: 0.5s;
+}
+
+.reseaux a {
+    position: relative;
+    display: block;
+    width: 40px;
+    height: 40px;
+    background: #fff;
+    text-align: center;
+    transform: perspective(1000px) rotate(-30deg) skew(25deg) translate(0, 0);
+    transition: 0.5s;
+    transition-timing-function: linear;
+    box-shadow: -20px 20px 10px rgba(0, 0, 0, 0.5);
+}
+
+
+.reseaux a:before {
+    content: "";
+    position: absolute;
+    top: 10px;
+    left: -20px;
+    height: 100%;
+    width: 20px;
+    background: #b1b1b1;
+    transition: 0.5s;
+    transform: rotate(0deg) skewY(-45deg);
+
+}
+
+.reseaux a:after {
+    content: "";
+    position: absolute;
+    bottom: -20px;
+    left: -10px;
+    height: 20px;
+    width: 100%;
+    background: #b1b1b1;
+    transition: 0.5s;
+    transform: rotate(0deg) skewX(-45deg);
+}
+
+.reseaux a:hover {
+    transform: perspective(1000px) rotate(-30deg) skew(25deg) translate(20px, -20px);
+    box-shadow: -50px 50px 50px rgba(0, 0, 0, 0.5);
+}
+
+.reseaux li:hover .fa-brands {
+    color: white;
+}
+
+.reseaux li:hover a {
+    background: var(--clr0);
+}
+
+.reseaux li:hover a:before {
+    background: var(--clr1);
+}
+
+.reseaux li:hover a:after {
+    background: var(--clr2);
+}
+
+
+
+
 
 </style>
--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,157 +1,326 @@
 <script>
+
+document.addEventListener('DOMContentLoaded', function() {
+  // Attend que la page soit entièrement chargée
+
+  // Sélectionne l'élément que vous souhaitez animer
+  var headerChild1 = document.querySelector('.header-child-1');
+
+  // Ajoute une classe pour déclencher l'animation
+  headerChild1.classList.add('animate-on-load');
+});
+
+
 </script>
 
 <template>
+
   <div>
+    
     <header id="header">
 
-      <div class="div-logo">
-        <img src="../images/logo2.png" alt="logo">
-      </div>
+      <div class="header-child-1 animate-on-load">
 
-      <div class="div-menu">
+        <div class="div-logo">
+          <img src="../images/logo3.png" alt="logo">
+        </div>
 
-        <nav>
-            <a href="#">Accueil</a>
-            <a href="#">Services</a>
-            <a href="#">Contact</a>
-        </nav>
+        <!-- <div class="moving-text">Votre agence de création de site web<span class="span-noir">------------------------------------------------------------</span>Votre agence de création de site web<span class="span-noir">--------------------------------------------------</span>Votre agence de création de site web<span class="span-noir">--------------------------------------------------</span>Votre agence de création de site web<span class="span-noir">--------------------------------------------------</span>Votre agence de création de site web<span class="span-noir">--------------------------------------------------</span>Votre agence de création de site web<span class="span-noir">--------------------------------------------------</span>Votre agence de création de site web<span class="span-noir">--------------------------------------------------</span>Votre agence de création de site web<span class="span-noir">--------------------------------------------------</span>Votre agence de création de site web<span class="span-noir">--------------------------------------------------</span>Votre agence de création de site web<span class="span-noir">--------------------------------------------------</span>Votre agence de création de site web<span class="span-noir">--------------------------------------------------</span>Votre agence de création de site web<span class="span-noir">--------------------------------------------------</span>Votre agence de création de site web<span class="span-noir">--------------------------------------------------</span>Votre agence de création de site web<span class="span-noir">--------------------------------------------------</span>Votre agence de création de site web<span class="span-noir">--------------------------------------------------</span>Votre agence de création de site web<span class="span-noir">--------------------------------------------------</span>Votre agence de création de site web<span class="span-noir">--------------------------------------------------</span>Votre agence de création de site web<span class="span-noir">--------------------------------------------------</span>Votre agence de création de site web<span class="span-noir">--------------------------------------------------</span>Votre agence de création de site web<span class="span-noir">--------------------------------------------------</span>Votre agence de création de site web<span class="span-noir">--------------------------------------------------</span>Votre agence de création de site web<span class="span-noir">--------------------------------------------------</span>Votre agence de création de site web<span class="span-noir">--------------------------------------------------</span>Votre agence de création de site web<span class="span-noir">--------------------------------------------------</span>Votre agence de création de site web<span class="span-noir">--------------------------------------------------</span>Votre agence de création de site web<span class="span-noir">--------------------------------------------------</span>Votre agence de création de site web<span class="span-noir">--------------------------------------------------</span>Votre agence de création de site web<span class="span-noir">--------------------------------------------------</span>Votre agence de création de site web<span class="span-noir">--------------------------------------------------</span>Votre agence de création de site web<span class="span-noir">--------------------------------------------------</span>Votre agence de création de site web<span class="span-noir">--------------------------------------------------</span>Votre agence de création de site web<span class="span-noir">--------------------------------------------------</span>Votre agence de création de site web<span class="span-noir">--------------------------------------------------</span>Votre agence de création de site web<span class="span-noir">--------------------------------------------------</span>Votre agence de création de site web<span class="span-noir">--------------------------------------------------</span>Votre agence de création de site web<span class="span-noir">--------------------------------------------------</span>Votre agence de création de site web<span class="span-noir">--------------------------------------------------</span>Votre agence de création de site web<span class="span-noir">--------------------------------------------------</span>Votre agence de création de site web<span class="span-noir">--------------------------------------------------</span>Votre agence de création de site web<span class="span-noir">--------------------------------------------------</span>Votre agence de création de site web<span class="span-noir">--------------------------------------------------</span>Votre agence de création de site web<span class="span-noir">--------------------------------------------------</span>Votre agence de création de site web<span class="span-noir">--------------------------------------------------</span>Votre agence de création de site web<span class="span-noir">--------------------------------------------------</span>Votre agence de création de site web<span class="span-noir">--------------------------------------------------</span>Votre agence de création de site web<span class="span-noir">--------------------------------------------------</span>Votre agence de création de site web<span class="span-noir">--------------------------------------------------</span>Votre agence de création de site web<span class="span-noir">--------------------------------------------------</span>Votre agence de création de site web<span class="span-noir">--------------------------------------------------</span>Votre agence de création de site web<span class="span-noir">--------------------------------------------------</span>Votre agence de création de site web<span class="span-noir">--------------------------------------------------</span>Votre agence de création de site web<span class="span-noir">--------------------------------------------------</span>Votre agence de création de site web<span class="span-noir">--------------------------------------------------</span>Votre agence de création de site web<span class="span-noir">--------------------------------------------------</span>Votre agence de création de site web<span class="span-noir">--------------------------------------------------</span>Votre agence de création de site web</div> -->
+       
 
-        <!-- <div>
-            <ul>
-            <li style="--i:6;" data-icon=""> 
-                <a href="#">Accueil</a>
-            </li>
-            <li style="--i:5;" data-icon=""> 
-                <a href="#">Services</a>
-            </li>
-            <li style="--i:4;" data-icon=""> 
-                <a href="#">Contact</a>
-            </li>
-        </ul>
+        <!-- <div class="tag-list">
+          <div class="inner">
+            <div class="tag"><span></span>Votre</div>
+            <div class="tag"><span></span>Agence</div>
+            <div class="tag"><span></span>de</div>
+            <div class="tag"><span></span>Création</div>
+            <div class="tag"><span></span>de</div>
+            <div class="tag"><span></span>Site</div>
+            <div class="tag"><span></span>Web</div>
+
+            <div class="tag"><span></span>Votre agence de création de site web</div>
+          </div>
+
+          <div class="fade"></div>
+
         </div> -->
+
         
 
       </div>
 
+      <div class="header-child-2">
+
+        <p>Votre agence de création de site web</p>
+
+        
+
+        
+
+          <!-- <div>
+              <ul>
+              <li style="--i:6;" data-icon=""> 
+                  <a href="#">Accueil</a>
+              </li>
+              <li style="--i:5;" data-icon=""> 
+                  <a href="#">Services</a>
+              </li>
+              <li style="--i:4;" data-icon=""> 
+                  <a href="#">Contact</a>
+              </li>
+          </ul>
+          </div> -->
+          
+
+        
+
+      </div>
+
+
+
+
+
+      <div class="header-child-3">
+        <div class="div-menu">
+
+          <nav>
+              <a href="#"><span>ACCUEIL</span></a>
+              <a href="#"><span>SERVICES</span></a>
+              <a href="#"><span>À PROPOS DE NOUS</span></a>
+              <a href="#"><span>CONTACT</span></a>
+          </nav>
+        </div>
+
+
+
+
+      </div>
+
     </header>
+
   </div>
 </template>
 
-<style>
+<style scoped>
 @import url(https://fonts.googleapis.com/css?family=Oswald:400,700);
 @import url(https://use.fontawesome.com/releases/v6.4.2/css/all.css);
 
 header {
-  background-color: #050816;
-  height: 300px;
+  background-color: black;
+  height: 20em;
   width: 100%;
-  border-bottom: solid 1px white;
+  /* border-bottom: solid 0.5px gray; */
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
+}
+
+.span-noir {
+  color: black;
+}
+
+
+
+/* .tag-list {
+  width: 100%;
+  font-family: "Montserrat";
+  position: relative;
+  overflow: hidden;
+}
+
+.inner {
+  width: 100%;
+  display: flex;
+  gap: 10rem;
+  animation: loop 15s linear infinite;
+}
+
+.tag {
+  
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0 0rem;
+  color: #e2e8f0;
+  font-size: 1.5em;
+  background-color: black;
+  padding: 0.7rem 1rem;
+  border-radius: 0.4rem;
+  box-shadow: 0 0.1rem 0.2rem #00000033, 0 0.1rem 0.5rem #0000004d, 0 0.2rem 1.5rem #00000066;
+
+}
+
+.tag span {
+  font-size: 1.2rem;
+  color: #64748b;
+}
+
+.fade {
+  position: absolute;
+  background: linear-gradient(90deg, #000000, transparent 30%, transparent 70%, #000000);
+  inset: 1;
+  pointer-events: none;
+}
+
+@keyframes loop {
+  0% {
+    transform: translateX(100%);
+  }
+
+  100% {
+    transform: translateX(-1%);
+  }
+}
+
+
+.animate-on-load {
+  height: 4em;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  position: relative;
+  overflow: hidden;
+}
+
+.animate-on-load .moving-text {
+  color: white;
+  font-size: 2em;
+  position: absolute;
+  top: 50%;
+  left: 100%;
+  white-space: nowrap;
+  animation: moveText 30s linear infinite;
+  animation-delay: 0s;
+}
+
+@keyframes moveText {
+  0% {
+    transform: translateX(100%);
+  }
+  100% {
+    transform: translateX(-1000%);
+  }
+  101% {
+    transform: translateX(100%);
+  }
+} */
+
+
+
+
+.header-child-1 {
+  height: 8em;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  
+
+}
+
+
+.header-child-1 {
+  width: 100%;
+}
+
+/* .header-child-1 .moving-text {
+  color: white;
+  font-size: 2em;
+  position: absolute;
+  top: 50%; 
+  left: 100%;
+  white-space: nowrap;
+  animation: moveText 200s linear infinite; 
+  animation-delay: 0s; 
+
+}
+
+@keyframes moveText {
+  0% {
+    transform: translateX(100%);
+  }
+  100% {
+    transform: translateX(-1000%);
+  }
+  101% {
+    transform: translateX(100%);
+  }
+} */
+
+
+
+.header-child-2 {
+  height: 4em;
+  /* border-top: solid 0.1px gray; */
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+}
+
+.header-child-2 p {
+  color: white;
+  font-size: 1.6em;
+
+}
+
+.header-child-3 {
+  height: 8em;
+  /* border-top: solid 0.1px gray; */
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
 }
 
 .div-logo {
-  width: 50%;
+  width: 30%;
   display: flex;
   align-items: center;
   padding-left: 1.5em;
 }
 
 .div-menu {
-  width: 50%;
+  width: 70%;
   display: flex;
   justify-content: end;
   align-items: center;
   text-align: center;
 }
 
-/* .div-menu div {
-    display: flex;
-    justify-content: end;
-} */
-
-/* code menu */
-/* ul {
-  position: relative;
-  transform: skewY(-5deg);
-  margin: 3em;
-  
-}
-
-li {
-  position: relative;
-  list-style: none;
-  width: 80px;
-  height: 10px;
-  padding: 0.5em;
-  background: #B62EEB;
-  z-index: calc(1 * var(--i));
-  transition: 0.5s;
-}
-
-li:hover {
-  background: #B62EEB;
-  transform: translateX(-50px);
-}
-
-li::before {
-  font-family: "FontAwesome";
-  color: #999;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  content: attr(data-icon);
-  position: absolute;
-  top: 0;
-  left: -40px;
-  width: 40px;
-  height: 100%;
-  background: #B62EEB;
-  transform-origin: right;
-  transform: skewY(45deg);
-  transition: 0.5s;
-}
-
-li:hover::before {
-  background: #B62EEB;
-}
-
-li::after {
-  content: "";
-  position: absolute;
-  top: -40px;
-  left: 0px;
-  width: 100%;
-  height: 40px;
-  background: #B62EEB;
-  transform-origin: bottom;
-  transform: skewX(45deg);
-  transition: 0.5s;
-}
-
-li:hover::after {
-  background: #B62EEB;
-}
-
-li a {
+nav a {
   text-decoration: none;
-  color: #999;
-  display: block;
-  text-transform: uppercase;
-  letter-spacing: 0.05em;
-  transition: 0.5s;
+  color: white;
+  margin-right: 5em;
+  padding: 0.5em;
+  /* background-color: #B62EEB; */
+  border-radius: 20px;
+  font-family: 'Cardo', serif;
 }
 
-li:hover a {
-  color: #fff;
+nav a span {
+    display: inline-block; /* Ajoutez cette règle pour que le span soit un élément en bloc */
 }
 
-li:last-child::after {
-  box-shadow: -120px 120px 20px rgba(0, 0, 0, 0.25);
+/* nav a span:hover {
+    
+    transform: scale(1.3); 
+    transition: transform 1s ease; 
 } */
+
+
 
 .div-logo img {
-  width: 9em;
+  width: 6.5em;
 }
+
+.div-logo img:hover {
+  transform: rotate(0deg) scale(0.5) translateX(2px) translateY(-4px); /* Ajustez les valeurs selon vos préférences */
+  transition: transform 0.3s ease-in-out;
+}
+
 </style>
